89099c10c166767d60d17a71a179705b0d9679ec
SkyPooZi <fadane294@gmail.com>
2023-08-11 19:09:05 +0700

    style:[SZ]add textview

--- lib/Film.dart
+++ lib/Film.dart
@@ -1,9 +1,18 @@
-class Siswa{
-  late String name;
-  late String email;
+class Film {
+  late String nameFilm;
+  late String Date;
+  late String Genre;
+  late String Durasi;
+  late String Rating;
+  late String Poster;
 
-  Siswa(String name, String email){
-    this.name = name;
-    this.email = email;
+
+  Film(String nameFilm, String Date, String Genre, String Durasi, String Rating, String Poster){
+    this.nameFilm = nameFilm;
+    this.Date = Date;
+    this.Genre = Genre;
+    this.Durasi = Durasi;
+    this.Rating = Rating;
+    this.Poster = Poster;
   }
 }
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,20 +10,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Siswa>? siswa11PPLG2 = [];
+  List<Film>? FilmFavorite = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Siswa joseph = new Siswa("Joseph", "joseph@gmail.com");
-    Siswa arvi = new Siswa("Arvi", "arvi@gmail.com");
-    Siswa adam = new Siswa("Adam", "adam@gmail.com");
-    Siswa damar = new Siswa("Damar", "damar@gmail.com");
-    siswa11PPLG2?.add(joseph);
-    siswa11PPLG2?.add(arvi);
-    siswa11PPLG2?.add(adam);
-    siswa11PPLG2?.add(damar);
+    Film Up = new Film("Up", "29 Juli 2009 (Indonesia)", "Anak-anak/Petualangan", "1 j 36 m", "8,3/10", 'assets/up.jpg');
+    Film bigHero6 = new Film("Big Hero 6", "7 November 2014 (Indonesia)", "Anak-anak/Petualangan", "1 j 42 m", "7,8/10", 'assets/bighero6.jpg');
+    Film Wall_E = new Film("WALL•E", "13 Agustus 2008 (Indonesia)", "Petualangan/Anak-anak", "1 j 38 m", "8,4/10", 'assets/walle.jpg');
+    Film Coco = new Film("Coco", "22 November 2017 (Indonesia)", "Anak-anak/Petualangan", "1 j 45 m", "8,4/10", 'assets/coco.jpg');
+    Film insideOut = new Film("Inside Out", "19 Agustus 2015 (Indonesia)", "Anak-anak/Komedi", "1 j 35 m", "8,1/10", 'assets/insideout.jpg');
+    Film Ratatouille = new Film("Ratatouille", "28 Juni 2007 (Indonesia)", "Anak-anak/Komedi", "1 j 51 m", "8,1/10", 'assets/ratatouille.jpg');
+    Film Moana = new Film("Moana", "25 November 2016 (Indonesia)", "Anak-anak/Musikal", "1 j 47 m", "7,6/10", 'assets/moana.jpg');
+    Film How_to_Train_Your_Dragon = new Film("How to Train Your Dragon", "20 Maret 2010 (Indonesia)", "Anak-anak/Petualangan", "1 j 38 m", "8,1/10", 'assets/howtotrainyourdragon.jpg');
+    Film Wreck_It_Ralph = new Film("Wreck-It Ralph", "9 November 2012 (Indonesia)", "Anak-anak/Petualangan", "1 j 41 m", "7,7/10", 'assets/wreckitralph.jpg');
+    Film theIncredibles = new Film("The Incredibles", "24 Oktober 2004 (Amerika)", "Anak-anak/Petualangan", "1 j 55 m", "8/10", 'assets/theincredibles.jpg');
+    Film findingNemo = new Film("Finding Nemo", "30 Mei 2003 (Amerika)", "Anak-anak/Petualangan", "1 j 40 m", "8,2/10", 'assets/findingnemo.jpg');
+    Film Cars = new Film("Cars", "9 Juni 2006 (Amerika)", "Komedi/Anak-anak", "1 j 57 m", "7,2/10", 'assets/cars.jpg');
+    Film toyStory = new Film("Toy Story", "22 November 1995 (Amerika)", "Anak-anak/Petualangan", "1 j 21 m", "8,3/10", 'assets/toystory.jpg');
+    Film monstersInc = new Film("Monsters, Inc.", "2 November 2001 (Amerika)", "Komedi/Petualangan", "1 j 32 m", "8,1/10", 'assets/monsterinc.jpg');
+    Film Soul = new Film("Soul", "11 Oktober 2020", "Anak-anak/Petualangan", "1 j 40 m", "8/10", 'assets/soul.jpg');
+    FilmFavorite?.add(Up);
+    FilmFavorite?.add(bigHero6);
+    FilmFavorite?.add(Wall_E);
+    FilmFavorite?.add(Coco);
+    FilmFavorite?.add(insideOut);
+    FilmFavorite?.add(Ratatouille);
+    FilmFavorite?.add(Moana);
+    FilmFavorite?.add(How_to_Train_Your_Dragon);
+    FilmFavorite?.add(Wreck_It_Ralph);
+    FilmFavorite?.add(theIncredibles);
+    FilmFavorite?.add(findingNemo);
+    FilmFavorite?.add(Cars);
+    FilmFavorite?.add(toyStory);
+    FilmFavorite?.add(monstersInc);
+    FilmFavorite?.add(Soul);
   }
 
   @override
@@ -33,16 +55,20 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Container(
           child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: siswa11PPLG2?.length,
+              padding: const EdgeInsets.all(10),
+              itemCount: FilmFavorite?.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
-                  height: 50,
+                  height: 200,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(siswa11PPLG2![index].name),
-                      Text(siswa11PPLG2![index].email),
+                      Image(image: AssetImage(FilmFavorite![index].Poster), width: 80, height: 80,),
+                      Text(FilmFavorite![index].nameFilm,),
+                      Text("Tanggal Rilis : " + FilmFavorite![index].Date),
+                      Text("Genre : " + FilmFavorite![index].Genre),
+                      Text("Durasi : " + FilmFavorite![index].Durasi),
+                      Text("Rating : " + FilmFavorite![index].Rating),
                     ],
                   ),
                 );
